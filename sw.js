25f704e4ab83c32fee1988207765d4cfd9ef1f9f
rahulk39377-bit <rahulk39377@gmail.com>
2026-08-16 00:39:53 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'desi-bazar-v1';
+const urlsToCache = [
+  '/Desi-Bazar/',
+  '/Desi-Bazar/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(name => name !== CACHE_NAME)
+             .map(name => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('/Desi-Bazar/index.html'));
+    })
+  );
+});
